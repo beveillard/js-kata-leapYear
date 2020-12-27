5441f83916036429bc5b9169fada74b653e69dd5
Benoît Eveillard <benoit.eveillard@gmail.com>
2020-12-27 17:50:54 -0500

    feature(leapYear) implement

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,7 +1,21 @@
-import { systemUnderTest } from ".";
+import { leapYear } from ".";
 
-describe("Test of systemUnderTest()", function() {
-  it("Should pass", function() {
-    expect(systemUnderTest()).toEqual(true);
+describe("Test of leapYear()", function () {
+
+  it("2000 is a leap year", function () {
+    expect(leapYear(2000)).toEqual(true);
   });
+
+  it("1900 is not a leap year", function () {
+    expect(leapYear(1900)).toEqual(false);
+  });
+
+  it("2020 is a leap year", function () {
+    expect(leapYear(2020)).toEqual(true);
+  });
+
+  it("2011 is not a leap year", function () {
+    expect(leapYear(2011)).toEqual(false);
+  });
+
 });
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
-export function systemUnderTest(): boolean {
-  return true;
+export function leapYear(year) {
+  if (year % 400 == 0) return true;
+  if (year % 100 == 0) return false;
+  if (year % 4 == 0) return true;
+  return false;
 }
