cd83aeb23c8383dc06f1e25f2e43fb8c8a8ea36c
Benoît Eveillard <benoit.eveillard@gmail.com>
2021-09-18 09:10:14 -0400

    LeapYear

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,7 +1,35 @@
-import { systemUnderTest } from ".";
+import { leapYear } from ".";
 
-describe("Test of systemUnderTest()", function() {
-  it("Should pass", function() {
-    expect(systemUnderTest()).toEqual(true);
+describe("Test of leapYear()", function () {
+
+  it("2021 isn't a leap year", function () {
+    expect(leapYear(2021)).toEqual(false);
   });
+
+  it("2020 is a leap year", function () {
+    expect(leapYear(2020)).toEqual(true);
+  });
+
+  it("2024 is a leap year", function () {
+    expect(leapYear(2024)).toEqual(true);
+  });
+
+  it("1900 isn't a leap year", function () {
+    expect(leapYear(1900)).toEqual(false);
+  });
+
+  it("2100 isn't a leap year", function () {
+    expect(leapYear(2100)).toEqual(false);
+  });
+
+  it("2000 i a leap year", function () {
+    expect(leapYear(2000)).toEqual(true);
+  });
+
+  it("1600 i a leap year", function () {
+    expect(leapYear(1600)).toEqual(true);
+  });
+
+
+
 });
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
-export function systemUnderTest(): boolean {
-  return true;
+export function leapYear(y) {
+  if (isMultipleOf(400, y)) return true;
+  if (isMultipleOf(100, y)) return false;
+  if (isMultipleOf(4, y)) return true;
+  return false;
+}
+
+function isMultipleOf(m, y) {
+  return y % m === 0;
 }
